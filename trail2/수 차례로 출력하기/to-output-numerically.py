90304b729338ad869554139a06caef20a5a8c3fd
kimkihyun1 <83591685+kimkihyun1@users.noreply.github.com>
2026-09-20 01:44:31 +0900

    [Passed] 수 차례로 출력하기 (50ms, 16MB)

---
+++ trail2/수 차례로 출력하기/to-output-numerically.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+# Please write your code here.
+def print_num(n):
+    if n == 0:
+        return
+
+    print_num(n-1)
+    print(n, end=" ")
+
+def print_reverse_num(n):
+    if n == 0:
+        return
+
+    print(n, end=" ")
+    print_reverse_num(n-1)
+
+print_num(n)
+print()
+print_reverse_num(n)

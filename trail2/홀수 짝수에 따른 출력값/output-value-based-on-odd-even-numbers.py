1be38854e0e532765df229c4048a39dbb6b15fc9
kimkihyun1 <83591685+kimkihyun1@users.noreply.github.com>
2026-09-23 10:09:32 +0900

    [Passed] 홀수 짝수에 따른 출력값 (47ms, 16MB)

---
+++ trail2/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+# Please write your code here.
+def odd_even(n):
+    if n % 2 == 0:
+        if n == 2:
+            return 2
+        return odd_even(n - 2) + n
+    
+    else:
+        if n == 1:
+            return 1
+        return odd_even(n - 2) + n
+
+print(odd_even(N))
